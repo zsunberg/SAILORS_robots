9bb2ae40abb55918aa679983138e70b1f225f025
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-07-18 10:00:35 -0700

    made sailorsbot more robust

--- sailorsbot.py
+++ sailorsbot.py
@@ -14,6 +14,7 @@ class SBot(object):
         self._shared['right_motor'] = Value('d',0.0)
         self._shared['sensors'] = Array('i', 5*[0])
         self._shared['line_position'] = Value('d',0.0)
+        self._shared['kill_flag'] = Value('i', 0)
         self._comm = Process(target=scomm, args=(id_num, self._shared))
         self._comm.start()
 
@@ -21,13 +22,15 @@ class SBot(object):
         self.direct_pipe.send(string)
 
     def kill_comm(self):
-        self._comm.terminate()
+        self._shared['kill_flag'].value = 1
         self._comm.join()
 
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            print exc_type, exc_value, traceback
         self.stop()
         self.kill_comm()
 
@@ -62,16 +65,29 @@ def scomm(id_num, shared):
     s.connect((ip_addr, 9750))
     s.settimeout(0.01)
 
+    errors = []
+    stopped_with_ctrl_c = False
+
+    def send_commands():
+        if shared['direct_pipe'].poll():
+            s.send(shared['direct_pipe'].recv())
+
+        s.send('l:{}\n'.format(shared['left_motor'].value))
+        s.send('r:{}\n'.format(shared['right_motor'].value))
+
+
     while True:
 
         try:
+            if shared['kill_flag'].value != 0:
+                break
+
             time.sleep(0.2)
 
-            if shared['direct_pipe'].poll():
-                s.send(shared['direct_pipe'].recv())
+            if shared['left_motor'].value != 0.0 or shared['right_motor'].value != 0.0:
+                stopped_with_ctrl_c = False
 
-            s.send('l:{}\n'.format(shared['left_motor'].value))
-            s.send('r:{}\n'.format(shared['right_motor'].value))
+            send_commands()
 
             data = None
             try:
@@ -79,8 +95,6 @@ def scomm(id_num, shared):
             except socket.timeout, e:
                 pass
             
-            # print(data)
-
             if data is not None:
                 for d in data.split('\n'):
                     try:
@@ -96,11 +110,23 @@ def scomm(id_num, shared):
                         else:
                             print(d)
                     except (IndexError, ValueError) as e:
-                        warn(str(e))
+                        # warn(str(e))
+                        # errors.append(e)
+                        if len(errors) >= 5:
+                            warn("5 communication errors:")
+                            while errors:
+                                print(str(errors.pop(0)))
+
 
         except KeyboardInterrupt as e:
-            shared['left_motor'].value = 0.0
-            shared['right_motor'].value = 0.0
+            if not stopped_with_ctrl_c:
+                shared['left_motor'].value = 0.0
+                shared['right_motor'].value = 0.0
+                send_commands()
+                stopped_with_ctrl_c = True
+            else: # already stopped with ctrl-C
+                send_commands()
+                break
 
-    # will never get here - hopefully things will end up ok if the process is terminated
+    send_commands()
     s.close()
