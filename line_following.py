b821ab6b29d8ac3f6299a3cb1923e602749d1ce3
Lauren Yang <laurenyang@Laurens-MacBook-Pro.local>
2015-07-20 16:48:36 -0700

    updating the line following files

--- line_following.py
+++ line_following.py
@@ -1,7 +1,18 @@
 from sailorsbot import SBot
 from line_follow_functions import follow_segment
+import pdb
 
-with SBot(3) as car:
+""" use python -m pdb myscript.py in the terminal to test debugging.
+    follow this link for common commands:
+    
+    http://stackoverflow.com/questions/4929251/can-you-step-through-python-code-to-help-debug-issues
+    
+    this command sets a breakpoint on the following line:
+    pdb.set_trace()
+"""
+
+
+with SBot(13) as car:
     print "created car"
     follow_segment(car)
 
