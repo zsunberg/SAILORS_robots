69ef8397421a8652be45a2582a40e36b7d39248b
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-08-09 18:01:35 -0700

    switched to tcp, added automatic connection reset and instructions

--- sailorsbot.py
+++ sailorsbot.py
@@ -4,7 +4,7 @@ from warnings import warn
 import time
 import sys
 
-#from ipdb import set_trace as bp
+# from ipdb import set_trace as bp
 
 MANUAL_MODE = 0;
 LINE_FOLLOWING_MODE = 1;
@@ -18,8 +18,8 @@ class SBot(object):
         # self._shared['direct_pipe'], self.direct_pipe = Pipe(False)
         self._shared['direct_pipe'], self.direct_pipe = Pipe(False)
         self._shared['data_tick'] = Value('l',-1) # incremented every time data is received
-        self._shared['left_motor'] = Value('d',0.0)
-        self._shared['right_motor'] = Value('d',0.0)
+        # self._shared['left_motor'] = Value('d',0.0)
+        # self._shared['right_motor'] = Value('d',0.0)
         self._shared['sensors'] = Array('i', 5*[0])
         self._shared['line_position'] = Value('d',0.0)
         self._shared['kill_flag'] = Value('i', 0)
@@ -49,12 +49,16 @@ class SBot(object):
 
     def stop(self):
         self.set_mode(MANUAL_MODE, wait_for_ack=False)
-        self._shared['left_motor'].value = 0.0        
-        self._shared['right_motor'].value = 0.0
+        self.direct_send('l:0.0\n')
+        self.direct_send('r:0.0\n')
+        # self._shared['left_motor'].value = 0.0        
+        # self._shared['right_motor'].value = 0.0
 
     def forward(self, speed):
-        self._shared['right_motor'].value = speed
-        self._shared['left_motor'].value = speed
+        self.direct_send('l:{}\n'.format(speed))
+        self.direct_send('r:{}\n'.format(speed))
+        # self._shared['right_motor'].value = speed
+        # self._shared['left_motor'].value = speed
 
     def get_sensors(self):
         return list(self._shared['sensors'])
@@ -63,27 +67,33 @@ class SBot(object):
         return self._shared['line_position'].value
 
     def set_left_motor(self, speed):
-        self._shared['left_motor'].value = speed
+        self.direct_send('l:{}\n'.format(speed))
 
     def set_right_motor(self, speed):
-        self._shared['right_motor'].value = speed
+        self.direct_send('r:{}\n'.format(speed))
 
     def set_mode(self, mode, wait_for_ack=True):
         last_time = self._shared['mode_ack_time'].value
         self.direct_send('c:{}\n'.format(mode))
         if wait_for_ack:
-            while self._shared['mode_ack_time'].value <= last_time and self._shared['reported_mode'].value != mode:
+            while self._shared['mode_ack_time'].value <= last_time:
                 time.sleep(0.02)
 
     def turn(self, direction):
         last_time = self._shared['mode_ack_time'].value
         self.direct_send('t:{}\n'.format(direction))
-        while self._shared['mode_ack_time'].value <= last_time and self._shared['reported_mode'].value != TURN_MODE:
+        while self._shared['mode_ack_time'].value <= last_time:
             time.sleep(0.02)
+        self.wait_for_manual()
 
     def nudge(self, duration):
+        last_time = self._shared['mode_ack_time'].value
         self.direct_send("n:{}\n".format(duration))
         time.sleep(duration+0.2)
+        while self._shared['mode_ack_time'].value <= last_time:
+            time.sleep(0.02)
+        self.wait_for_manual()
+
 
     def wait_for_manual(self):
         while self._shared['reported_mode'].value != MANUAL_MODE:
@@ -96,27 +106,44 @@ class SBot(object):
 
 
 def scomm(id_num, shared):
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    # ip_addr = '192.168.1.1{:02d}'.format(id_num)
-    # s.connect((ip_addr, 9750))
-    # s.settimeout(0.01)
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ip_addr = '192.168.1.1{:02d}'.format(id_num)
-    s.bind(('', 9700+id_num))
-    s.settimeout(0.01)
 
+    def setup_connection():
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            ip_addr = '192.168.1.1{:02d}'.format(id_num)
+            s.connect((ip_addr, 9750))
+            s.settimeout(0.01)
+        except socket.error as e:
+            print e
+            if e[0]==111: # connection refused
+                print
+                print 'if in ipython: press Ctrl-C twice quickly and try again'
+                print 'otherwise: press Ctrl-C, run `pkill python` and try again'
+                print
+                sys.exit()
+            else:
+                raise e
+        return s
+
+    def reset_connection(s):
+        print
+        print 'automatically resetting connection. please wait...'
+        s.close()
+        s = setup_connection()
+        print 'done.'
+        print
+        return s
+
+    s = setup_connection()
 
     errors = []
-    ticks_since_ctrl_c = 10
+    ticks_since_ctrl_c = 20
     leftovers = ''
+    reset_flag = False
 
-    def send_commands():
-        if shared['direct_pipe'].poll():
-            s.sendto(shared['direct_pipe'].recv(), (ip_addr,9750))
-
-        s.sendto('l:{}\n'.format(shared['left_motor'].value), (ip_addr,9750))
-        s.sendto('r:{}\n'.format(shared['right_motor'].value), (ip_addr,9750))
-
+    def send_commands(s):
+        while shared['direct_pipe'].poll():
+            s.send(shared['direct_pipe'].recv())
 
     while True:
 
@@ -124,21 +151,32 @@ def scomm(id_num, shared):
             if shared['kill_flag'].value != 0:
                 break
 
-            time.sleep(0.1)
 
-            '''
-            if shared['left_motor'].value != 0.0 or shared['right_motor'].value != 0.0:
-                stopped_with_ctrl_c = 0
-            '''
+            time.sleep(0.05)
+
             ticks_since_ctrl_c += 1
 
-            send_commands()
+            try:
+                send_commands(s)
+            except socket.error as e:
+                print e
+                if e[0]==104: # connection reset
+                    s = reset_connection(s)
+                else:
+                    raise e
+
 
             data = None
             try:
                 data = leftovers + s.recv(1024)
             except socket.timeout, e:
                 pass
+            except socket.error as e:
+                print e
+                if e[0]==104: # connection reset
+                    s = reset_connection(s)
+                else:
+                    raise e
 
             if data is not None:
                 with shared['data_tick'].get_lock():
@@ -170,27 +208,25 @@ def scomm(id_num, shared):
                     except (IndexError, ValueError) as e:
                         if len(d) > 0:
                             warn(str(e)+'\ndata was "{}"'.format(d))
-                        # errors.append(e)
-                        # if len(errors) >= 5:
-                        #     warn("{} communication errors:".format(len(errors)))
-                        #     while errors:
-                        #         print(str(errors.pop(0)))
-
 
         except KeyboardInterrupt as e:
-            if ticks_since_ctrl_c > 10:
-                s.sendto('c:{}\n'.format(MANUAL_MODE), (ip_addr,9750))
-                shared['left_motor'].value = 0.0
-                shared['right_motor'].value = 0.0
-                send_commands()
+            if ticks_since_ctrl_c > 20:
+                s.send('c:{}\n'.format(MANUAL_MODE))
+                s.send('l:0.0\n')
+                s.send('r:0.0\n')
+                send_commands(s)
                 ticks_since_ctrl_c = 0
             else: # already stopped with ctrl-C
-                send_commands()
+                send_commands(s)
                 break
 
-    send_commands()
+    send_commands(s)
     s.close()
-    # sys.stdout.write('waiting for connection to close...')
-    # # sys.stdout.flush()
-    # time.sleep(2)
-    # sys.stdout.write('done\n')
+
+
+# for udp
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ip_addr = '192.168.1.1{:02d}'.format(id_num)
+# s.bind(('', 9700+id_num))
+# s.settimeout(0.01)
+
