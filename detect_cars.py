980197c0cb02b5c58b829749e61cb81bedd80617
Zachary Sunberg <sunbergzach@gmail.com>
2017-06-15 14:48:53 -0700

    added detect_cars

---
+++ detect_cars.py
@@ -0,0 +1,10 @@
+from sailorsbot import SBot
+from sailorsmap import RoadMap
+from my_graph_functions import shortest_path
+from graph_util import turns
+import time
+
+graph = RoadMap('map_in_ASL.json')
+while True:
+    graph.detect_cars()
+    print graph.detect_cars
